b79f3582248bda827b40aee1a7f250162d77bfe3
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-07-07 10:08:37 +0200

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import unionIcon from "../../assets/icons/union.svg";
+
+function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <img src={unionIcon} alt="logo" />
+    </>
+  );
+}
+
+export default Home;
