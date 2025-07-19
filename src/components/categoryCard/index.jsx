07677267d59241c0729ac684a60c6e46121786ee
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-07-19 17:25:19 +0200

    added adaptive+responsive for the categoryCardList component

--- src/components/categoryCard/index.jsx
+++ src/components/categoryCard/index.jsx
@@ -5,14 +5,14 @@ function CategoryCard({ icon, title, isShowMore = false }) {
   const cardClasses = `${styles.card} ${isShowMore ? styles.showMore : ""}`;
 
   return (
-    <div className={cardClasses}>
+    <a href="#" className={cardClasses}>
       {isShowMore ? (
         <img src={arrowIcon} alt="Больше категорий" className={styles.icon} />
       ) : (
         <img src={icon} alt={title} className={styles.icon} />
       )}
       <p className={styles.title}>{title}</p>
-    </div>
+    </a>
   );
 }
 
