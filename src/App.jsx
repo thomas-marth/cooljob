182a20ba86b00e707abd0702af13e5621ffba7b0
Dmytro Bazhenov <63171863+thomas-marth@users.noreply.github.com>
2025-07-20 17:40:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import LanguageSwitcher from "./components/languageSwitcher";
-import TextDisplay from "./components/textDisplay";
-import LanguageContext from "./contexts/LanguageContext";
+import "./App.css";
+import Home from "./pages/home/index";
 
 function App() {
-  const [language, setLanguage] = useState("en");
-
-  const toggleLanguage = () => {
-    setLanguage((prev) => (prev === "en" ? "ru" : "en"));
-  };
-
-  const contextValue = {
-    language,
-    toggleLanguage,
-  };
-
   return (
-    <LanguageContext.Provider value={contextValue}>
-      <div className="container">
-        <TextDisplay />
-        <LanguageSwitcher />
-      </div>
-    </LanguageContext.Provider>
+    <>
+      <Home />
+    </>
   );
 }
 
